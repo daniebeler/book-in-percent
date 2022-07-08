9ba6b6b653b6c03a805014db3512d82d4392932c
daniebeler <hiebeler.daniel@gmail.com>
2022-07-08 14:35:48 +0200

    implemented basic algorithms

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { RangeCustomEvent } from '@ionic/angular';
+import { RangeValue } from '@ionic/core';
 
 @Component({
   selector: 'app-home',
@@ -7,6 +9,17 @@ import { Component } from '@angular/core';
 })
 export class HomePage {
 
+  lastEmittedValue: any = 0;
+  percentage = 0;
+  totalPages = 355;
+
   constructor() {}
+
+  onIonChange(ev: Event) {
+    this.lastEmittedValue = (ev as RangeCustomEvent).detail.value;
+    if (!isNaN(this.lastEmittedValue)) {
+      this.percentage = Math.floor(this.lastEmittedValue / this.totalPages * 100);
+    }
+  }
 
 }
